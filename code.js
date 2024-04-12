fc54ea21a8b8f69457bccf08bbddeb3464acf9be
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-04-12 17:25:27 -0600

    Adding my bottomUpEuler and exporting them all for testing

--- code.js
+++ code.js
@@ -7,3 +7,22 @@ function e(n) {
     if(n === 0) return 1;
     else return 1.0 / factorial(n) + e(n - 1);
 }
+
+
+
+function bottomUpEuler(n) {
+    if (n === 0)
+        return 1;
+    else {
+        let fact = 1, e = 1;
+        for (let i = 1; i <= n; i++) {
+            fact = fact * i;
+            e = e + 1.0/fact; 
+        }
+        return e;
+    }
+}
+
+
+//add so i can test functions
+module.exports = {bottomUpEuler, e, factorial};
